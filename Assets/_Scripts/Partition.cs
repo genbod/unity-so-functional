136e94c79ddfa0796823656d1172477126ceb6e3
Ben Godard (ANALOG) <bgodard@microsoft.com>
2018-05-29 22:04:07 +0000

    Merged PR 402: Initial pull request
    added parameters file so that we can have different settings per release.

---
+++ Assets/_Scripts/Partition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Partition : MonoBehaviour
+{
+    public AnimatedFill Fill;
+    public Text Text;
+}
---
+++ Assets/_Scripts/TextUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TextUtils
+{
+    public static string GetPrettyNumber(float totalCount)
+    {
+        if (totalCount > 100000)
+        {
+            var millions = totalCount / 1000000;
+            return millions.ToString("0.00") + "M";
+        }
+        return (totalCount / 1000).ToString("0.00") + "K";
+    }
+}
