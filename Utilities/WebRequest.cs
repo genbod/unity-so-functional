f1b56332ed22334552563a91853ad3e6941e230f
Ben Godard <bgodard@outlook.com>
2018-10-06 23:30:25 -0700

    working image importer

--- Utilities/WebRequest.cs
+++ Utilities/WebRequest.cs
@@ -16,7 +16,7 @@ public class WebRequest {
 
     public static IEnumerator GetWebText(Url url)
     {
-        yield return GetWebText(url, Enumerable.Empty<Tuple<string, string>>());
+        return GetWebText(url, Enumerable.Empty<Tuple<string, string>>());
     }
 
 	public static IEnumerator GetWebText(Url url, IEnumerable<Tuple<string, string>> headers, bool treatEmptyStringAsError = false)
@@ -25,6 +25,10 @@ public class WebRequest {
         www.chunkedTransfer = false;
         headers.ForEach(t => www.SetRequestHeader(t.Item1, t.Item2));
         yield return www.SendWebRequest();
+        while (!www.isDone)
+        {
+            yield return null;
+        }
 
         if (www.isNetworkError || www.isHttpError)
         {
@@ -36,6 +40,35 @@ public class WebRequest {
             yield return Exceptional.Of<string>(new WebRequestException("Result was empty"));
         }
         else yield return www.downloadHandler.text;
+    }
+
+    public static IEnumerator GetWebTexture(Url url)
+    {
+        return GetWebTexture(url, Enumerable.Empty<Tuple<string, string>>());
+    }
+
+    public static IEnumerator GetWebTexture(Url url, IEnumerable<Tuple<string, string>> headers)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url.ToString());
+        www.chunkedTransfer = false;
+        headers.ForEach(t => www.SetRequestHeader(t.Item1, t.Item2));
+        yield return www.SendWebRequest();
+        while (!www.isDone)
+        {
+            yield return null;
+        }
+        
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield return Exceptional.Of<Texture2D>(new WebRequestException(www.error));
+        }
+        else
+        {
+            //Texture retTexture = DownloadHandlerTexture.GetContent(www);
+            //yield return retTexture;
+            yield return www.downloadHandler.data;
+        }
     }
 }
 
