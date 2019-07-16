357a9a257171eab86cbbe5f783f107e974da48f1
genbod <bgodard@outlook.com>
2019-07-15 21:06:19 -0700

    fixed trigger

--- Utilities/AnimationTriggerSetter.cs
+++ Utilities/AnimationTriggerSetter.cs
@@ -12,20 +12,27 @@ public class AnimationTriggerSetter : SerializedMonoBehaviour
     [Tooltip("SetTrigger Function from Animator you want to call")]
     public Action<int> AnimationSetter;
 
+    [SerializeField]
+    private Animator _animator;
+
     private string _currentState;
 
     private void Start()
     {
         _currentState = ValueGetter();
-        AnimationSetter(Animator.StringToHash(_currentState));
+        if (_animator != null)
+        {
+            _animator.SetTrigger(Animator.StringToHash(_currentState));
+        }
     }
 
     private void Update()
     {
-        if (ValueGetter() != _currentState)
+        var newState = ValueGetter();
+        if (newState != _currentState && !_animator.GetBool(Animator.StringToHash(newState)))
         {
             _currentState = ValueGetter();
-            AnimationSetter(Animator.StringToHash(_currentState));
+            _animator.SetTrigger(Animator.StringToHash(_currentState));
         }
     }
 }
