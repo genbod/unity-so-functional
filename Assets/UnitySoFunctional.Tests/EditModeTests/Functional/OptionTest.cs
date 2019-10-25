d4d595505615f76bfcf64e8a679b532143bcb644
genbod <bgodard@outlook.com>
2019-10-24 19:07:18 -0700

    WIP initial play tests

---
+++ Assets/UnitySoFunctional.Tests/EditModeTests/Functional/OptionTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using static F;
+
+namespace Tests
+{
+    public class OptionTest
+    {
+        [Test]
+        public void TestSomeInt_EqualsSomeIntWithSameValue()
+        {
+            var testOption = Some(3);
+            Assert.That(testOption, Is.EqualTo(Some(3)));
+        }
+
+        [Test]
+        public void TestMatch_CallsAppropriateFunc()
+        {
+            Assert.That(3, Is.EqualTo(GetValue(Some(3))));
+            Assert.That(-1, Is.EqualTo(GetValue(None)));
+        }
+
+        private int GetValue(Option<int> value)
+        => value.Match(
+            Some: n => n,
+            None: () => -1
+        );
+    }
+}
