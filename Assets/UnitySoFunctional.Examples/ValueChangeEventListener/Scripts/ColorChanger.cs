264a446fb2ffd893e8332123a2149a732f9cb2a7
Ben Godard <bgodard@outlook.com>
2019-11-15 10:22:22 -0800

    working test scene

---
+++ Assets/UnitySoFunctional.Examples/ValueChangeEventListener/Scripts/ColorChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    [SerializeField]
+    Material material;
+
+    public void ChangeColor(string htmlColor)
+    {
+        if (material != null)
+        {
+            Color newColor;
+            ColorUtility.TryParseHtmlString(htmlColor, out newColor);
+            material.SetColor("_Color", newColor);
+        }
+    }
+}
